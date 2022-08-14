42d8851d1ebfd1a65e6b43a012997671efd0ec7b
Ричард Кайтуков <30377534+casfonki@users.noreply.github.com>
2022-08-14 18:33:30 +0300

    'depl'

--- build/src/main.js
+++ build/src/main.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var args = process.argv;
-console.log(args);
+const tslib_1 = require("tslib");
+const args_1 = tslib_1.__importDefault(require("args"));
+args_1.default
+    .option('key', 'API key')
+    .option('name', 'name of the miniapp', 'Tuse test');
+var flags = args_1.default.parse(process.argv);
+console.log(`key=${flags.key}`);
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
-var args = process.argv;
-console.log(args);
+
+
+import args from 'args';
+
+args
+  .option('key', 'API key')
+  .option('name', 'name of the miniapp','Tuse test')
+  
+
+var flags = args.parse(process.argv)
+
+console.log(`key=${flags.key}`)

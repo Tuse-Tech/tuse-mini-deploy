a1dd6b451c65ca8221b3a60ca70e88cc32d82001
Ричард Кайтуков <30377534+casfonki@users.noreply.github.com>
2022-08-15 00:57:05 +0300

    'depl'

--- build/src/main.js
+++ build/src/main.js
@@ -4,13 +4,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 const args_1 = tslib_1.__importDefault(require("args"));
 const adm_zip_1 = tslib_1.__importDefault(require("adm-zip"));
-const fs_1 = tslib_1.__importDefault(require("fs"));
+const form_data_1 = tslib_1.__importDefault(require("form-data"));
+const axios_1 = require("axios");
 args_1.default
     .option('key', 'API key')
     .option('name', 'name of the miniapp', 'Tuse test');
 var flags = args_1.default.parse(process.argv);
 var folder = process.cwd();
 var zip = new adm_zip_1.default();
-var s = fs_1.default.readdirSync(folder);
-console.log(s);
+zip.addLocalFolder(folder);
+var bodyFormData = new form_data_1.default();
+bodyFormData.append('image', zip);
+new axios_1.Axios().request({
+    method: "post",
+    url: "http://tuse.tech:9001/api/miniapps",
+    data: bodyFormData,
+    headers: { "Content-Type": "multipart/form-data" },
+})
+    .then(function (response) {
+    //handle success
+    console.log(response);
+})
+    .catch(function (response) {
+    //handle error
+    console.log(response);
+});
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@
 import args from 'args';
 import AdmZip from 'adm-zip'
 import fs from 'fs';
+import FormData from "form-data";
+import {Axios} from 'axios';
 args
   .option('key', 'API key')
   .option('name', 'name of the miniapp','Tuse test')
@@ -14,6 +16,20 @@ var flags = args.parse(process.argv)
 var folder = process.cwd();
 
 var zip  = new AdmZip()
-
-var s = fs.readdirSync(folder)
-console.log(s);
+zip.addLocalFolder(folder);
+var bodyFormData = new FormData();
+bodyFormData.append('image', zip); 
+new Axios().request({
+    method: "post",
+    url: "http://tuse.tech:9001/api/miniapps",
+    data: bodyFormData,
+    headers: { "Content-Type": "multipart/form-data" },
+  })
+    .then(function (response) {
+      //handle success
+      console.log(response);
+    })
+    .catch(function (response) {
+      //handle error
+      console.log(response);
+    });
